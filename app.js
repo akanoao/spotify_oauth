0de69f60ac15af779260c35c7bc826bf9e6aa127
akanoao <dakshgakhar18@gmail.com>
2024-11-04 20:17:30 +0530

    public path

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const stateKey = 'spotify_auth_state';
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(cookieParser());
 
